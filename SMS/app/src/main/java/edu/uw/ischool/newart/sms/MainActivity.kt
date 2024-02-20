3064dd4be5394fdd407fb4f1f5ed91d5e8278409
Ted Neward <ted@tedneward.com>
2024-02-19 23:53:09 -0800

    SMS example with full permission steps in place. Runs in emulator.

--- SMS/app/src/main/java/edu/uw/ischool/newart/sms/MainActivity.kt
+++ SMS/app/src/main/java/edu/uw/ischool/newart/sms/MainActivity.kt
@@ -1,11 +1,18 @@
 package edu.uw.ischool.newart.sms
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony
 import android.telephony.SmsManager
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,17 +25,72 @@ class MainActivity : AppCompatActivity() {
         btnSend.setOnClickListener {
             sendSMS()
         }
+        checkForSmsPermission()
     }
-
+    private fun checkForSmsPermission() {
+        // This will (probably) prompt only once, when first installed/run on the device.
+        // Once obtained, the permission will be "sticky".
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) !=
+            PackageManager.PERMISSION_GRANTED) {
+            Log.d("MainActivity", "Permission not granted!")
+            // Permission not yet granted. Use requestPermissions().
+            // MY_PERMISSIONS_REQUEST_SEND_SMS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            ActivityCompat.requestPermissions(
+                this, arrayOf<String>(Manifest.permission.SEND_SMS), 1)
+            btnSend.isEnabled = false
+        } else {
+            // Permission already granted. Enable the message button.
+            btnSend.isEnabled = true
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (permissions[0].equals(Manifest.permission.SEND_SMS, ignoreCase = true)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted.
+                } else {
+                    // Permission denied.
+                    Log.d("MainActivity", "Failed to obtain permission")
+                    Toast.makeText(
+                        this,
+                        "Failed to obtain permission",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    // Disable the message button.
+                    btnSend.isEnabled = false
+                }
+            }
+        }
+    }
     fun sendSMS() {
+        checkForSmsPermission()
+
         // We can either
         // (a) use the built-in SMS app to send the text message
-        /*
-        val sendIntent = Intent(Intent.ACTION_VIEW)
-        sendIntent.putExtra("Hello from an Android application!", "default content")
-        sendIntent.setType("vnd.android-dir/mms-sms")
+        //*
+        //val sendIntent = Intent(Intent.ACTION_VIEW)
+        val defaultSmsPackageName = Telephony.Sms.getDefaultSmsPackage(this) // Need to change the build to API 19
+
+        val sendIntent = Intent(Intent.ACTION_SEND)
+        sendIntent.setType("text/plain")
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hello there!")
+
+        if (defaultSmsPackageName != null)
+        // Can be null in case that there is no default,
+        // then the user would be able to choose
+        // any app that support this intent.
+        {
+            Log.i("MainActivity", "defaultSmsPackageName is $defaultSmsPackageName")
+            sendIntent.setPackage(defaultSmsPackageName)
+        }
         startActivity(sendIntent)
-         */
+        // */
 
         // (b) use the SmsManager API to send it from our app directly
         //val smsManager = SmsManager.getDefault()   // deprecated
